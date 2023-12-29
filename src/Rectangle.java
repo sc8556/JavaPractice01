44f62b37258f334559330b133ca88209e139f0d8
sc8556 <70990973+sc8556@users.noreply.github.com>
2023-12-29 16:06:31 +0900

    연습 3.2 - 생성자 추가
    연습 3.2 - 생성자 추가

--- src/Rectangle.java
+++ src/Rectangle.java
@@ -2,22 +2,32 @@ public class Rectangle {
     int width;
     int height;
 
+
     Rectangle(){
-        width = height = 1; // 너비, 높이 초기화, 너비와 높이가 0이면 존재하지 않는 것이기 때문에 1로 초기화
+        width = height = 1;
+        // 너비, 높이 초기화
+        // 너비와 높이가 0이면 존재하지 않는 것이기 때문에 1로 초기화
+    }
+    Rectangle(int width, int height){
+
+        this(); // 전달받은 너비와 높이가 음수일 경우 기본값으로 세팅, Rectangle() 생성자 호출
+        if(width > 0 ||  height > 0){  // 전달받은 값을 너비, 높이 변수에 각각 저장
+            this.width = width;
+            this.height = height;
+        }
+
+
+
+    }
+    // 전달받은 너비와 높이를 Return
+    String getArea(){
+        return width + " * " + height;
     }
 
     // 다른 클래스에서 이 생성자를 호출할 수 있도록 public으로 생성
-    public  Rectangle(int width, int h){
-        if(width < 0 ||  height < 0){
-            width = 1;
-            height = 1;
-        }
 
-        this.width = width;
-        height = h;
-    }
-
-    int calArea(){
+    int calArea() {
         return width * height;
+
     }
 }
